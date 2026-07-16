d7be44ca840cc551798c82eafbcfa730d60a1ab2
PersonaFang <fangjingmail@126.com>
2026-07-16 17:49:28 +0800

    feat:学习Promise异步操作+Async函数将异步操作变为同步操作; 学习Vue模板语法、渲染

---
+++ FRONTEND/04-vue/vue3-study/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h3>学习Vue:模板语法</h3>
+    <p>{{ message }}</p>
+    <div>{{ rawHtml }}</div>
+    <div v-html="rawHtml"></div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: "HelloWorld",
+  data(){
+    return {
+      message: "学习Vue",
+      rawHtml:"<a href='http://www.baidu.com'>百度</a>"
+    }
+  }
+}
+</script>
